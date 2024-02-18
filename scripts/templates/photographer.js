45e0592009a718190be27e1e50cdfc435d59d18e
Joshua Urdanabia <joshua.urdanabia@gmail.com>
2024-02-18 15:56:34 +0100

    Integrate URLSearchParams in photographer.js

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -3,29 +3,43 @@ export function photographerTemplate(data) {
 
     const picture = `assets/photographers/${portrait}`;
 
+    // Fonction pour créer les paramètres d'URL avec URLSearchParams
+    function createURLWithSearchParams() {
+        const params = new URLSearchParams();
+        params.set('id', id);
+        return params.toString();
+    }
+
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
+        const article = document.createElement('article');
 
-        const profilePic = document.createElement( 'img' );
-        profilePic.src = picture
-        profilePic.alt = name
+        // Créer un lien autour de l'article avec les paramètres d'URL
+        const link = document.createElement('a');
 
-        const identification = document.createElement( 'p' );
+        // Appeler la fonction pour obtenir les paramètres d'URL
+        const paramsString = createURLWithSearchParams();
+        link.href = `photographer.html?${paramsString}`;
+
+        const profilePic = document.createElement('img');
+        profilePic.src = picture;
+        profilePic.alt = name;
+
+        const identification = document.createElement('p');
         identification.textContent = id;
 
-        const naming = document.createElement( 'h2' );
+        const naming = document.createElement('h2');
         naming.textContent = name;
 
-        const cities = document.createElement('p')
+        const cities = document.createElement('p');
         cities.textContent = city;
 
-        const countries = document.createElement('p')
+        const countries = document.createElement('p');
         countries.textContent = country;
 
-        const description = document.createElement('p')
+        const description = document.createElement('p');
         description.textContent = tagline;
 
-        const pricing = document.createElement('p')
+        const pricing = document.createElement('p');
         pricing.textContent = price;
 
         article.appendChild(profilePic);
@@ -35,7 +49,12 @@ export function photographerTemplate(data) {
         article.appendChild(countries);
         article.appendChild(description);
         article.appendChild(pricing);
-        return (article);
+        link.appendChild(article);
+
+        // return (article);
+        console.log(link);
+        return (link);
     }
-    return { picture, id, name, city, country, tagline, price, getUserCardDOM }
+
+    return { picture, id, name, city, country, tagline, price, getUserCardDOM };
 }
