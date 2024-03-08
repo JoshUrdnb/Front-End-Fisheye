ad27791e2bb7f7620d3a9ff0fe811ac4a28ee99d
Joshua Urdanabia <joshua.urdanabia@gmail.com>
2024-03-08 09:27:40 +0100

    refactoring JS templates photographer.js and CSS for index.html

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -10,64 +10,72 @@ export function photographerTemplate(data) {
         return params.toString();
     }
 
-    // function filterHighLikesMedia() {
-    //     const thresholdLikes = 50;
-    //     return media.filter(mediaItem => mediaItem.likes > thresholdLikes);
-    // }
+    // Afficher les prix avec les centimes dans une devise precise :
+    const formatToCurrency = (price, money, country) => {
+    return new Intl.NumberFormat(country, {
+        style: 'currency',
+        currency: money,
+    }).format(price / 1);
+    }
 
     function getUserCardDOM() {
         const article = document.createElement('article');
-
+    
         // Créer un lien autour de l'article avec les paramètres d'URL :
         const link = document.createElement('a');
-
+    
         // Appeler la fonction pour obtenir les paramètres d'URL :
         const paramsString = createURLWithSearchParams();
         link.href = `photographer.html?${paramsString}`;
-
+    
         const profilePic = document.createElement('img');
         profilePic.src = picture;
         profilePic.alt = name;
-
+    
         const identification = document.createElement('p');
         identification.textContent = id;
-
+    
         const naming = document.createElement('h2');
         naming.textContent = name;
+    
+        // Ma nouvelle div pour englober les éléments <p>
+        const articleWrapper = document.createElement('div');
+        // articleWrapper.appendChild(identification); // Ne plus afficher l'id
 
-        const cities = document.createElement('p');
-        cities.textContent = city;
-
-        const countries = document.createElement('p');
-        countries.textContent = country;
-
+        const location = document.createElement('p');
+        location.textContent = `${city}, ${country}`;
+        articleWrapper.appendChild(location);
+        
+        // const cities = document.createElement('p');
+        // cities.textContent = city;
+        // articleWrapper.appendChild(cities);
+    
+        // const countries = document.createElement('p');
+        // countries.textContent = country;
+        // articleWrapper.appendChild(countries);
+    
         const description = document.createElement('p');
         description.textContent = tagline;
+        articleWrapper.appendChild(description);
 
         const pricing = document.createElement('p');
-        pricing.textContent = price;
-
-        // const highLikesMedia = filterHighLikesMedia();
-
-        // highLikesMedia.forEach(mediaItem => {
-        //     const mediaElement = document.createElement('div');
-        //     mediaElement.textContent = `Media: ${mediaItem.title}, Likes: ${mediaItem.likes}`;
-        //     article.appendChild(mediaElement);
-        // });
-
+        pricing.textContent = `${formatToCurrency(price, 'EUR', 'fr-FR')}`;
+        articleWrapper.appendChild(pricing);
+    
+        // const pricing = document.createElement('p');
+        // pricing.textContent = `${price}€/jours`;
+        // articleWrapper.appendChild(pricing);
+    
+        // Ajouter les éléments à la div
         article.appendChild(profilePic);
-        article.appendChild(identification);
         article.appendChild(naming);
-        article.appendChild(cities);
-        article.appendChild(countries);
-        article.appendChild(description);
-        article.appendChild(pricing);
+        article.appendChild(articleWrapper); // Ajouter la nouvelle div à l'article
         link.appendChild(article);
-
+    
         // return (article);
         console.log(link);
-        return (link);
+        return link;
     }
-
+    
     return { picture, id, name, city, country, tagline, price, media, getUserCardDOM };
 }
