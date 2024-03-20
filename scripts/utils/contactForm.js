8ae5b66d055c0746bec08e15431c7a79c9cf9280
Joshua Urdanabia <joshua.urdanabia@gmail.com>
2024-03-20 13:09:57 +0100

    Form validation complete on contactForm.js

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -2,7 +2,34 @@ function validate (event) {
     event.preventDefault()
     console.log('envoyé')
 
-    // inserer ici message erreurs avec alert
+    const form = event.target;
+    const first = form.querySelector('#first').value;
+    const last = form.querySelector('#last').value;
+    const email = form.querySelector('#email').value;
+    const message = form.querySelector('#newField').value;
+
+    let error = false;
+
+    if (first.trim() === '') {
+        error = true
+    }
+
+    if (last.trim() === '') {
+        error = true
+    }
+
+    if (email.trim() === '') {
+        error = true
+    }
+
+    if (message.trim() === '') {
+        error = true
+    }
+
+    if (error) {
+        alert('Veuillez remplir tous les champs du formulaire.')
+        return false
+    }
 }
 
 export function displayModal(name) {
